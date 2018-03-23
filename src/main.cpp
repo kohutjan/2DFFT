4bebceae711871d1d8db5336367686e1649c1efa
Jan Kohut <kohut.honza@gmail.com>
2018-03-23 22:00:39 +0100

    Seperate spec files.

--- src/main.cpp
+++ src/main.cpp
@@ -8,22 +8,29 @@ int main(int argc, char **argv)
 {
   static struct option long_options[] = {
   {"run", required_argument, 0, 'r'},
+  {"filters", required_argument, 0, 'f'},
   {0, 0, 0, 0}};
 
   string runFilePath;
+  string filtersFilePath;
 
   cout << endl;
   cout << "Params" << endl;
   cout << "#############################################################" << endl;
   int option_index = 0;
   int opt = 0;
-  while ((opt = getopt_long(argc, argv, "i:", long_options, &option_index)) != -1)
+  while ((opt = getopt_long(argc, argv, "r:f:", long_options, &option_index)) != -1)
   {
     switch (opt)
     {
-      case 'i':
+      case 'r':
         runFilePath = optarg;
         cout << "Run file path: " << optarg << endl;
+        break;
+
+      case 'f':
+        filtersFilePath = optarg;
+        cout << "Filters file path: " << optarg << endl;
         break;
 
       default:
@@ -34,9 +41,9 @@ int main(int argc, char **argv)
   cout << endl;
   cout << endl;
 
-  if (runFilePath.empty())
+  if (runFilePath.empty() || filtersFilePath.empty())
   {
-    cout << "Path for run file have to be set." << endl;
+    cout << "Paths to run and filters files have to be set." << endl;
     return -1;
   }
 
