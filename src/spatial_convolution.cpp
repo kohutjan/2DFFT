84efcd92461789e58393ef9c0dccb0f6bb082666
David Piskula <d.piskula@gmail.com>
2018-05-01 17:10:17 +0200

    Another bug in Regular, Separable still crashes

--- src/spatial_convolution.cpp
+++ src/spatial_convolution.cpp
@@ -9,9 +9,9 @@ void SpatialConvolution::Regular()
         for(int col = this->filter.cols / 2; col < this->src.cols - (this->filter.cols / 2); ++col)
 		{
 			double convolutionSum = 0;
-            for (int rowk = -(this->filter.rows / 2); rowk < (this->filter.rows / 2); ++rowk)
+            for (int rowk = -(this->filter.rows / 2); rowk <= (this->filter.rows / 2); ++rowk)
 			{
-                for (int colk = -(this->filter.cols / 2); colk < (this->filter.cols / 2); ++colk)
+                for (int colk = -(this->filter.cols / 2); colk <= (this->filter.cols / 2); ++colk)
 				{
                     convolutionSum += this->src.at<float>(row + rowk, col + colk) * this->filter.at<float>(rowk + (this->filter.rows / 2), colk + (this->filter.cols / 2));
 				}
@@ -29,9 +29,9 @@ void SpatialConvolution::Separable()
         for(int col = (this->kernelX.cols / 2); col < this->src.cols - (this->kernelX.cols / 2); ++col)
         {
             double convolutionSum = 0;
-            for (int colk = -(this->kernelX.cols / 2); colk < (this->kernelX.cols / 2); ++colk)
+            for (int colk = -(this->kernelX.cols / 2); colk <= (this->kernelX.cols / 2) - 1; ++colk)
             {
-                convolutionSum += this->src.at<float>(row, col + colk) * this->kernelX.at<float>(0, colk + (this->filter.cols / 2));
+                convolutionSum += this->src.at<float>(row, col + colk) * this->kernelX.at<float>(0, colk + (this->kernelX.cols / 2));
             }
             this->dst.at<float>(row, col) = convolutionSum;
         }
@@ -42,9 +42,9 @@ void SpatialConvolution::Separable()
         for(int row = (this->kernelY.rows / 2); row < this->dst.rows - (this->kernelY.rows / 2); ++row)
         {
             double convolutionSum = 0;
-            for (int rowk = -(this->kernelY.rows / 2); rowk < (this->kernelY.rows / 2); ++rowk)
+            for (int rowk = -(this->kernelY.rows / 2); rowk <= (this->kernelY.rows / 2) - 1; ++rowk)
             {
-                convolutionSum += this->dst.at<float>(row + rowk, col) * this->kernelY.at<float>(rowk + (this->filter.rows / 2), 0);
+                convolutionSum += this->dst.at<float>(row + rowk, col) * this->kernelY.at<float>(rowk + (this->kernelY.rows / 2), 0);
             }
             this->dst.at<float>(row, col) = convolutionSum;
         }
